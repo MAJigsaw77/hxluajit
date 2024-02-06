dfb4dddcb27334137510b082ef291d73751c0811
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-02-06 23:13:57 +0200

    Create MinGW-Building.sh

---
+++ project/luajit/scripts/MinGW-Building.sh
@@ -0,0 +1,26 @@
+# Author: Mihai Alexandru (MAJigsaw77)
+
+git clone https://github.com/LuaJIT/LuaJIT.git -b v2.1
+
+cd LuaJIT
+
+mkdir build
+
+# Window x64 MinGW
+
+mingw32-make clean
+mingw32-make -j8 HOST_CC="gcc -m64" TARGET_SYS=Windows
+cp src/libluajit.a build/libluajit-arm64.a
+
+# copy includes
+mkdir build/include
+
+cp src/lua.hpp build/include
+cp src/lauxlib.h build/include
+cp src/lua.h build/include
+cp src/luaconf.h build/include
+cp src/lualib.h build/include
+cp src/luajit.h build/include
+
+# go back
+cd ../
