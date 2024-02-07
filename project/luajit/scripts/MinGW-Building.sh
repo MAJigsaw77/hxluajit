918dabe9387ec3c823087279b164968bfa45674d
Lily <egzozu.be.bas@gmail.com>
2024-02-07 09:38:37 +0300

    Create MinGW-Building.sh

---
+++ project/luajit/scripts/MinGW-Building.sh
@@ -0,0 +1,27 @@
+# Author: Lily Ross (mcagabe19)
+# Note that use msys to build
+
+git clone https://github.com/LuaJIT/LuaJIT.git -b v2.1
+
+cd LuaJIT
+
+mkdir build
+
+cd src
+mingw32-make clean
+mingw32-make -j8
+cd ..
+cp src/libluajit.a build/libluajit.a
+
+# copy includes
+mkdir build/include
+
+cp src/lua.hpp build/include
+cp src/lauxlib.h build/include
+cp src/lua.h build/include
+cp src/luaconf.h build/include
+cp src/lualib.h build/include
+cp src/luajit.h build/include
+
+# go back
+cd ../
