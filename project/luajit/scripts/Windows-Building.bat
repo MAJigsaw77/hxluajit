6858d123555a7f92adb59e7a910f05c45ab993a4
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-04-25 07:14:14 +0300

    Update Windows-Building.bat

--- project/luajit/scripts/Windows-Building.bat
+++ project/luajit/scripts/Windows-Building.bat
@@ -1,17 +1,5 @@
 @echo off
 
 git clone https://github.com/LuaJIT/LuaJIT.git -b v2.1
-cd LuaJIT
-mkdir build
-mkdir build\include
-cd src
+cd LuaJIT/src
 msvcbuild.bat static
-cd ..
-cd ..
-copy LuaJIT\src\lua.hpp LuaJIT\build\include
-copy LuaJIT\src\lauxlib.h LuaJIT\build\include
-copy LuaJIT\src\lua.h LuaJIT\build\include
-copy LuaJIT\src\luaconf.h LuaJIT\build\include
-copy LuaJIT\src\lualib.h LuaJIT\build\include
-copy LuaJIT\src\luajit.h LuaJIT\build\include
-copy LuaJIT\src\lua51.lib LuaJIT\build
